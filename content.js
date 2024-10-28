dac0fe7c60c67d55b5fbf4a8540cfdfe80454786
Christine Brovkina <christine@whateverism.de>
2024-10-28 18:04:28 +0100

    rewrite blur function wrt scroll offset

--- content.js
+++ content.js
@@ -4,13 +4,13 @@ console.log("Hello from content.js");
 chrome.runtime.onMessage.addListener(gotMessage);
 
 // If Message is received, check if it is active
-function gotMessage(message, sender, sendResponse) {
-  let summary = {};
+function gotMessage(message) {
 
   if (message.isActive) {
     var mouseX, mouseY, distance;
     // Create a tooltip element
     const tooltip = document.createElement('div');
+    tooltip.id = 'tooltip';
     tooltip.style.position = 'absolute';
     tooltip.style.backgroundColor = 'transparent';
     tooltip.style.border = '1px solid red';
@@ -26,55 +26,74 @@ function gotMessage(message, sender, sendResponse) {
     document.body.appendChild(tooltip);
 
     const tooltipContent = document.createElement('div');
+    tooltipContent.id = 'tooltipContent';
     tooltipContent.style.position = 'absolute';
-    tooltipContent.style.backgroundColor = 'transparent';
+    tooltipContent.style.backgroundColor = 'whitesmoke';
     tooltipContent.style.color = 'red';
-    tooltipContent.style.fontSize = '.5rem';
+    tooltipContent.style.padding = '5px';
+    tooltipContent.style.fontSize = '12px';
+    tooltipContent.style.borderRadius = '5px';
     document.body.appendChild(tooltipContent);
-
-    function updateTooltip() {
-      tooltip.style.left = (mouseX - 25) + 'px';
-      tooltip.style.top = (mouseY - 25) + 'px';
-      tooltipContent.style.left = (mouseX + 25) + 'px';
-      tooltipContent.style.top = (mouseY+ 25) + 'px';
-      tooltipContent.innerHTML = `X: ${mouseX}px, Y: ${mouseY}px. <br>
-     ${document.body.scrollWidth}px ✕ ${document.body.scrollHeight}px`;
-      requestAnimationFrame(updateTooltip);
-    }
-
-    requestAnimationFrame(updateTooltip);
 
     var elements = [];
 
-    elements = Array.from(document.querySelectorAll('a, button, input, textarea, select, [tabindex]:not([tabindex="-1"]), img, p, h1, h2, h3, h4, h5, h6, ol, ul, li'));
+    elements = Array.from(document.querySelectorAll('a, button, input, textarea, select, [tabindex]:not([tabindex="-1"]), img, svg, p, h1, h2, h3, h4, h5, h6, ol, ul, pre, table, blockquote, dd, figcaption, abbr, b, bdi, bdo, cite, code, data,  dfn, em, i, mark, q, strong, s, u, sub, sup, time, area, audio, map, video, iframe, object, embed, portal, canvas, caption, label, legend, progress, datalist, option'));
 
     elements.forEach(element => {
-      element.style.setProperty("filter", "blur(5px)");
+      element.style.setProperty("filter", "blur(10px)");
+      element.style.setProperty("transition", "filter 0.25s ease-in");
     })
 
 
-    document.onmousemove = function(e) {
+    document.onmousemove = function (e) {
       mouseX = e.pageX;
       mouseY = e.pageY;
 
       elements.forEach(element => {
         distance = calculateDistance(element, mouseX, mouseY);
-        if (distance > 10) {
-        
-        } else {
-          element.style.setProperty("filter", "blur(0)");
-        }
+
+        console.log(`element: ${element.tagName}, distance: ${distance}`);
+
+        const maxBlur = 10;
+        const minBlur = 0;
+        // Calculate the maximum possible distance from the center of the element to any point on the screen
+      const maxDistance = Math.sqrt(window.innerWidth * window.innerWidth + window.innerHeight * window.innerHeight);
+      const minDistance = 10;
+
+      if (distance > maxDistance) {
+        blurValue = maxBlur;
+      } else if (distance < minDistance) {
+        blurValue = minBlur;
+      } else {
+        blurValue = maxBlur * (distance / maxDistance);
+      }
+      element.style.setProperty("filter", `blur(${blurValue}px)`);
       });
+
     }
   }
+
+
+  function updateTooltip() {
+    tooltip.style.left = (mouseX - 25) + 'px';
+    tooltip.style.top = (mouseY - 25) + 'px';
+    tooltipContent.style.left = (mouseX + 30) + 'px';
+    tooltipContent.style.top = (mouseY + 30) + 'px';
+    tooltipContent.innerHTML = `Field area: ${document.body.scrollWidth}px ✕ ${document.body.scrollHeight}px<br> Current X: ${mouseX}px, Y: ${mouseY}px.`;
+    requestAnimationFrame(updateTooltip);
+  }
+
+  requestAnimationFrame(updateTooltip);
 }
+
 function calculateDistance(elem, mouseX, mouseY) {
   const rect = elem.getBoundingClientRect();
-  const elemX = rect.left + (rect.width / 2);
-  const elemY = rect.top + (rect.height / 2);
+  const elemX = rect.left + window.scrollX + (rect.width / 2);
+  const elemY = rect.top + window.scrollY + (rect.height / 2);
   const deltaX = mouseX - elemX;
   const deltaY = mouseY - elemY;
   return Math.floor(Math.sqrt(deltaX * deltaX + deltaY * deltaY));
 }
+
 
 
